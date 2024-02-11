f95debb838eea4b07dbda0d3ec50f8b519a95ac7
trungvuive <trung150502@gmail.com>
2024-02-11 08:38:36 +0700

    setup 2 project success test and code

---
+++ stringToCamel.App/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace stringToCamel.App;
+
+using System;
+
+public class Program
+{
+    public static string StringToCamel(string underscoreSep)
+    {
+        var tempOut = string.Empty;
+
+        var tempArrSubStr = underscoreSep.split("_");
+
+        if (tempArrSubStr.Length == 0)
+            return 0;
+    }
+
+    public statuc void 
+}
